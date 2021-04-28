b0a7a20c402f243dc7018c6e96a01cdfee3d3baf
Emmanuel Sérié <eserie@gmail.com>
2021-04-28 14:50:44 +0200

    simplify convert_to_tensor

--- eagerpy/convert.py
+++ eagerpy/convert.py
@@ -1,9 +1,10 @@
+import sys
 import numbers
 from functools import partial
 from typing import Any
 
-import numpy as np
 from jax.tree_util import tree_flatten, tree_unflatten
+from .astensor import _get_module_name
 
 
 def convert_to_tensor(data: Any, tensor_type: str) -> Any:
@@ -19,40 +20,30 @@ def convert_to_tensor(data: Any, tensor_type: str) -> Any:
     data
         data structure with converted tensors.
     """
+    name = _get_module_name(data)
+    m = sys.modules
+    if m not in ["numpy", "jax", "torch", "tensorflow"]:
+        # do not convert
+        return data
+
     if tensor_type == "tensorflow":
         import tensorflow as tf
-
-        if isinstance(data, np.datetime64):
-            # datetime not managed by tensorflow
-            return data
 
         return tf.convert_to_tensor(data)
 
     elif tensor_type == "torch":
         import torch
 
-        if isinstance(data, np.datetime64):
-            # datetime not managed by pytorch
-            return data
-        elif isinstance(data, np.ndarray) and data.dtype.type is np.str_:
-            return data
-        elif isinstance(data, numbers.Number):
-            return data
-        new_data = torch.tensor(data)
-        return new_data
+        return torch.tensor(data)
 
     elif tensor_type == "jax":
-        import jax.numpy as jnp
+        import jax.numpy as np
 
-        if isinstance(data, np.datetime64):
-            # datetime not managed by jax
-            return data
-        elif isinstance(data, np.ndarray) and data.dtype.type is np.str_:
-            return data
-        new_data = jnp.asarray(data, dtype=data.dtype)
-        return new_data
+        return np.asarray(data, dtype=data.dtype)
 
     elif tensor_type == "numpy":
+        import numpy as np
+
         return np.asarray(data)
 
     raise ValueError(
